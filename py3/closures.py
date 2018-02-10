422c776250cb7ef07ad3169b42707ae59ad94b5f
Savio Abuga <savioabuga@gmail.com>
2018-02-10 03:33:41 +0300

    introduction

--- py3/closures.py
+++ py3/closures.py
@@ -1,9 +1,14 @@
 def outer_func(message):
+    """
+    outer_func returns a closure function
+    :param message: Some message
+    :return: cl
+    """
     def inner_func():
         print(message)
+
     return inner_func
 
 
-func = outer_func('Hi there')
+func = outer_func()
 print(func())
-
