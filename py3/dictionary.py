c5bddaa6cc0fb832346774a23bb4921baa718c10
Savio Abuga <savioabuga@gmail.com>
2018-02-10 12:56:39 +0300

    done with dictionaries

---
+++ py3/dictionary.py
@@ -0,0 +1,25 @@
+import collections
+route = {'id': 271, 'title': 'Fast app'}
+
+new = {}
+for k, v in route.items():
+    new[v] = k
+
+
+def print_items(**items):
+    print(items)
+
+
+print_items(**route)
+
+
+# Accessing data in a dict
+data = {
+    "year": 2001, "country": "Kenya"
+}
+
+
+data = collections.defaultdict(lambda: "MISSING", data)
+print(data["rating"])
+
+
