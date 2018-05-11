ff446292d5ce7c538f880ffb5968193de5cb5127
Savio Abuga <savioabuga@gmail.com>
2018-05-11 16:39:20 +0300

    using finializer

--- py3/phonebook/phonebook.py
+++ py3/phonebook/phonebook.py
@@ -1,10 +1,20 @@
+from pathlib import Path
+
 
 class Phonebook:
     def __init__(self):
         self.entries = {}
+        self.filename = 'filename.txt'
+        self.file_cache = open(self.filename, 'w')
 
     def add(self, name, number):
         self.entries[name] = number
 
     def lookup(self, name):
         return self.entries[name]
+
+    def clear(self):
+        self.entries = {}
+        self.file_cache.close()
+        f = Path(self.filename)
+        f.unlink()
--- py3/phonebook/test_phonebook.py
+++ py3/phonebook/test_phonebook.py
@@ -1,13 +1,23 @@
-import unittest
-from phonebook import Phonebook
+import pytest
+from phonebook.phonebook import Phonebook
 
 
-class PhonebookTest(unittest.TestCase):
+@pytest.fixture
+def phonebook():
+    phonebook = Phonebook()
+    yield phonebook
+    phonebook.clear()
 
-    def test_create_phonebook(self):
-        phonebook = Phonebook() # noqa
 
-    def test_lookup_entry_by_name(self):
-        phonebook = Phonebook()
-        phonebook.add('Bob', '12345')
-        self.assertEqual("12345", phonebook.lookup("Bob"))
+def test_lookup_entry_by_name(phonebook):
+    phonebook.add('Bob', '12345')
+    assert "12345" == phonebook.lookup("Bob")
+
+
+def test_missing_entry_raise_keyerror(phonebook):
+    with pytest.raises(KeyError):
+        phonebook.lookup('missing')
+
+
+# def test_empty_phonebook_is_constistent():
+#     self.assertTrue(self.phonebook.is_consistent())
